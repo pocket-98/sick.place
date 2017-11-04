9bcbde5f83268c4bdf32ecd69899ec280f29166b
Nolan Scobie <nolan.scobie@gmail.com>
2017-11-04 18:53:30 -0400

    regenerated with mvc skeleton

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -6,8 +6,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace backend
 {
@@ -33,8 +31,19 @@ namespace backend
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
 
-            app.UseMvc();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
