b9c02f4e9203603c8ae42895946c9fe82f621cf2
syz99 <samzyao@gmail.com>
2017-11-04 21:45:12 -0400

    no

--- www/script_blank.js
+++ www/script_blank.js
@@ -1,8 +1,7 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/syz99/cj9m17jqw24e92ro32edptmca/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3l6OTkiLCJhIjoiY2o5bTE4NmE0MGxtczJxbjQ1cTVlMGlnOCJ9.xFRVGwr05gnSzErdeDTHpw', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1Ijoic3l6OTkiLCJhIjoiY2o5bTE3ZjJyNHA4NjMzbHNsdjZ1ZzJjZSJ9.O7McJy9z5ioFmv3mxfYyYw'
-}).addTo(map);
+var map = L.map('map',{
+    center:[5,28],
+    zoom: 3,
+    minZoom:2,
+    maxZoom:18
+});
+L.tileLayer('https://api.mapbox.com/styles/v1/syz99/cj9m17jqw24e92ro32edptmca.html?fresh=true&title=true&access_token=pk.eyJ1Ijoic3l6OTkiLCJhIjoiY2o5bTE3ZjJyNHA4NjMzbHNsdjZ1ZzJjZSJ9.O7McJy9z5ioFmv3mxfYyYw#10.8/40.745772/-74.030553/0',{attribution:"Mapbox"}).addTo(map);
