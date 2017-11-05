5f421fdf9e058a544690e9db5d1a58a4c936a9e7
D4RK0V3RL0RD676 <dayalpavan@gmail.com>
2017-11-05 03:25:08 -0500

    lol

--- www/pavan/script.js
+++ www/pavan/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
+	//make button follow mouse
 	$(window).on("mousemove swipe click tap" , function(e) { updateButtonPosition(e.pageX, e.pageY); });
 	updateButtonPosition(window.innerWidth/2, window.innerHeight/2);
+
+	//initialize map
+	main.map = L.map("map");
+	L.mapbox.accessToken = "pk.eyJ1Ijoic3l6OTkiLCJhIjoiY2o5bTE3ZjJyNHA4NjMzbHNsdjZ1ZzJjZSJ9.O7McJy9z5ioFmv3mxfYyYw";
+	var url = "https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=" + L.mapbox.accessToken;
+	var copyright = { attribution: '\xA9 <a href="https://www.mapbox.com/feedback/">Mapbox</a> \xA9 <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' };
+	var mapboxTiles = L.tileLayer(url, copyright);
+	main.map.on('load', pullNewPoints);
+	main.map.addLayer(mapboxTiles).setView([35.9132, -79.0558], 15);
+	main.map.on('moveend', pullNewPoints); //called on move and zoom
 });
 
 function updateButtonPosition(x, y) {
@@ -63,3 +74,22 @@ function submitReport() {
 	}
 }
 
+function pullNewPoints() {
+    bounds = main.map.getBounds();
+    p1 = bounds.getSouthWest()
+    p2 = bounds.getNorthEast();
+
+    var request = {
+        lat1: p1.lat,
+        lon1: p1.lng,
+        lat2: p2.lat,
+        lon2: p2.lng,
+    };
+
+    $.get("http://localhost:5000/api/sicknessinarea", request, function(sickPoints) {
+        console.log(sickPoints);
+		        for (var i in sickPoints) {
+        		    injectPoint(sickPoints[i]);
+				 }
+    });
+}
