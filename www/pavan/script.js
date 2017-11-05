488a1c5a838ab4e2e2f037a844d24b7f46ec721d
D4RK0V3RL0RD676 <dayalpavan@gmail.com>
2017-11-05 02:35:51 -0500

    get req

--- www/pavan/script.js
+++ www/pavan/script.js
@@ -35,11 +35,31 @@ function checkSeverity() {
 		error = "Error: Severity must be a number";
 	}
 	transitionElement("#severity-error", errorHTML(error), 80);
+	return error;
 }
 
 function submitReport() {
-	var latitude = $("#latitude").val();
-	var longitude = $("#longitude").val();
-	var severity = $("#severity").val();
+	var latitude = parseFloat($("#latitude").val());
+	var longitude = parseFloat($("#longitude").val());
+	var severity = parseFloat($("#severity").val());
+	if (!checkSeverity() && latitude && longitude) {
+		request = {
+			latitude: latitude,
+			longitude: longitude,
+			severity: severity,
+			sicknessType: 0
+		};
+		$.get("api/asdf", request, function(data) {
+			if (data) {
+				$("#report-sickness-modal").modal("hide");
+			} else {
+				error = errorHTML("Error: Internal server error");
+				transitionElement("#severity-error", error, 200);
+			}
+		});
+	} else {
+		error = errorHTML("Error: Latitude or longitude or severity not defined");
+		transitionElement("#severity-error", error, 200);
+	}
 }
 
