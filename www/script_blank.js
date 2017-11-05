f1e6d5af5d8949c35e698b5bb802bdb23417d1ba
syz99 <samzyao@gmail.com>
2017-11-04 23:49:28 -0400

    i tried

--- www/script_blank.js
+++ www/script_blank.js
@@ -1,7 +1,4 @@
-var map = L.map('map',{
-    center:[5,28],
-    zoom: 3,
-    minZoom:2,
-    maxZoom:18
-});
-L.tileLayer('https://api.mapbox.com/styles/v1/syz99/cj9m17jqw24e92ro32edptmca.html?fresh=true&title=true&access_token=pk.eyJ1Ijoic3l6OTkiLCJhIjoiY2o5bTE3ZjJyNHA4NjMzbHNsdjZ1ZzJjZSJ9.O7McJy9z5ioFmv3mxfYyYw#10.8/40.745772/-74.030553/0',{attribution:"Mapbox"}).addTo(map);
+var map = L.map('map').setView([51, -0.00], 13);
+
+L.tileLayer('https://{s}.tiles.mapbox.com/v4/pk.eyJ1Ijoic3l6OTkiLCJhIjoiY2o5bTE3ZjJyNHA4NjMzbHNsdjZ1ZzJjZSJ9.O7McJy9z5ioFmv3mxfYyYw/{z}/{y}/{x}.png', {
+    attribution: "Mapbox"}).addTo(mymap);
